bad03529683abc37ace698da1ca1e0d52297c0f3
Nimish299 <f20201857@hyderabad.bits-pilani.ac.in>
2024-03-20 22:19:08 +0530

    Your descriptive commit message

--- back/routes/PlayerRoute.js
+++ back/routes/PlayerRoute.js
@@ -1,35 +1,47 @@
-const express=require('express')
-const router =express.Router()
+const express = require('express');
+const router = express.Router();
 
-const {
-  playermiddle
-} =require ('../middleware/playerMiddleware')
+const { playermiddle } = require('../middleware/playerMiddleware');
 
 const {
   signup,
   login,
   logout,
+  profile,
+  updateProfile,
   applied,
   applytoacad,
   leaveacad,
   addtostarred,
   starred,
-  removefromstarred
-} =require('../controllers/playerController')
+  removefromstarred,
+} = require('../controllers/playerController');
 
-router.post('/signup',signup)
-router.post('/login',login)
-router.get('/logout',logout)
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/logout', logout);
 
+router.use(
+  [
+    '/applytoacad',
+    '/applied',
+    '/leaveacad',
+    '/profile',
+    '/addtostarred',
+    '/starred',
+    '/removefromstarred',
+    '/updateProfile',
+  ],
+  playermiddle
+);
+router.get('/profile', profile);
+router.put('/updateProfile', updateProfile);
+router.post('/applytoacad', applytoacad);
+router.get('/applied', applied);
+router.delete('/leaveacad', leaveacad);
 
-router.use(['/applytoacad','/applied','/leaveacad','/addtostarred','/starred','/removefromstarred'],playermiddle)
-router.post('/applytoacad',applytoacad)
-router.get('/applied',applied)
-router.delete('/leaveacad',leaveacad)
+router.post('/addtostarred', addtostarred);
+router.get('/starred', starred);
+router.delete('/removefromstarred', removefromstarred);
 
-router.post('/addtostarred',addtostarred)
-router.get('/starred',starred)
-router.delete('/removefromstarred',removefromstarred)
-
-
-module.exports = router
+module.exports = router;
--- front/src/pages/player/PlayerSignup.js
+++ front/src/pages/player/PlayerSignup.js
@@ -1,115 +1,110 @@
-import { useNavigate, Link } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const PlayerSignup = () => {
-  const [emailID, setEmailID] = useState("");
-  const [password, setPassword] = useState("");
-  const [errDisplay, seterrDisplay] = useState("");
-  const [name,setName] = useState("");
-  const [cpassword,setCpassword] = useState("");
-  
+  const [emailID, setEmailID] = useState('');
+  const [password, setPassword] = useState('');
+  const [errDisplay, seterrDisplay] = useState('');
+  const [name, setName] = useState('');
+  const [cpassword, setCpassword] = useState('');
+
   const navigate = useNavigate();
 
-  const LoginFormSubmit = async (e) => {
-    if(cpassword==password) {
-    e.preventDefault();
-    const user = { emailID, password };
-    const response = await fetch(`/api/player/signup`, {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-    const json = await response.json();
+  const signupFormSubmit = async (e) => {
+    if (cpassword == password) {
+      e.preventDefault();
+      const user = { name, emailID, password };
+      const response = await fetch(`/api/player/signup`, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+      const json = await response.json();
 
-    if (response.ok) {
-      console.log(json);
-      return navigate("/player/home");
+      if (response.ok) {
+        console.log(json);
+        return navigate('/player/home');
+      } else {
+        console.log(json.error);
+        seterrDisplay(json.error);
+      }
     } else {
-      console.log(json.error);
-      seterrDisplay(json.error);
+      alert('Passwords do not match');
     }
-  }
-  else {
-    alert("Passwords do not match");
-  }
   };
 
   return (
     <>
-    <div  style={{
-      fontWeight: 'bold',
-      color: 'black', 
-      textAlign: 'center', 
-       
-    }} className="h-100 container  d-flex align-items-center justify-content-center">
-      <h2>Player Sign up</h2>
+      <div
+        style={{
+          fontWeight: 'bold',
+          color: 'black',
+          textAlign: 'center',
+        }}
+        className='h-100 container  d-flex align-items-center justify-content-center'
+      >
+        <h2>Player Sign up</h2>
       </div>
-    
 
-      <div style={{width:"350px"}} className= "h-100 container  d-flex align-items-center justify-content-center border my-2">
-        <form onSubmit={LoginFormSubmit}>
-          <div>
-            
-          </div>
-          
+      <div
+        style={{ width: '350px' }}
+        className='h-100 container  d-flex align-items-center justify-content-center border my-2'
+      >
+        <form onSubmit={signupFormSubmit}>
+          <div></div>
 
-            <div className="mb-3">
-            <label  className="form-label">
-              Name
-            </label>
+          <div className='mb-3'>
+            <label className='form-label'>Name</label>
             <input
-              type="text"
-              className="form-control"
-              id="name"
-              aria-describedby="emailHelp"
+              type='text'
+              className='form-control'
+              id='name'
+              aria-describedby='emailHelp'
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
-            
           </div>
 
-
-
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
+          <div className='mb-3'>
+            <label for='exampleInputEmail1' className='form-label'>
               Email address
             </label>
             <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
               value={emailID}
               onChange={(e) => setEmailID(e.target.value)}
             />
-            <div id="emailHelp" className="form-text">
+            <div id='emailHelp' className='form-text'>
               We'll never share your email with anyone else.
             </div>
           </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
+          <div className='mb-3'>
+            <label for='exampleInputPassword1' className='form-label'>
               Password
             </label>
             <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
+              type='password'
+              className='form-control'
+              id='exampleInputPassword1'
               value={password}
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
+          <div className='mb-3'>
+            <label for='exampleInputPassword1' className='form-label'>
               Confirm Password
             </label>
             <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
+              type='password'
+              className='form-control'
+              id='exampleInputPassword1'
               value={cpassword}
               onChange={(e) => {
                 setCpassword(e.target.value);
@@ -117,23 +112,19 @@ const PlayerSignup = () => {
             />
           </div>
 
-          <button type="submit" className="btn btn-primary">
+          <button type='submit' className='btn btn-primary'>
             Sign Up
           </button>
         </form>
 
         <div>{errDisplay && <p>{errDisplay}</p>}</div>
-        <div>
-          
-        </div>
+        <div></div>
       </div>
 
-<Link class="btn btn-primary" to="/" role="button">Back</Link>
-</>
-
-
-      
-    
+      <Link class='btn btn-primary' to='/' role='button'>
+        Back
+      </Link>
+    </>
   );
 };
 
